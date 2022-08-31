c95edfa299574af81cdc45804470e6cc46d14017
Lê Bảo Đăng <85282838+LeBaoDang@users.noreply.github.com>
2022-08-31 13:43:21 +0700

    update

--- src/main/java/com/websitebanhang/controller/admin/UserController.java
+++ src/main/java/com/websitebanhang/controller/admin/UserController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.websitebanhang.entitys.Users;
 import com.websitebanhang.service.UsersService;
@@ -22,7 +26,27 @@ public class UserController {
 	public String doGetIndex(Model model) {
 		List<Users> users = userService.findAll();
 		model.addAttribute("users",users);
+		model.addAttribute("userRequest", new Users());
 		return "admin/user";
+	}
+	
+	// /admin/user/delete?username={...}
+	@GetMapping("/delete")
+	public String doGetDelete(@RequestParam("username") String username,
+			RedirectAttributes redirectAttributes) {
+		try {
+			userService.deleteLogical(username);
+			redirectAttributes.addFlashAttribute("succeedMessage","User" + username + "was deleted");
+		} catch (Exception e) {
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("errorMessage","Cannot delete user" + username );
+		}
+		return "redirect:/admin/user";
+	}
+	
+	@PostMapping("/create")
+	public String doPostUserRequest(@ModelAttribute("userRequest") Users userRequest) {
+		
 	}
 	
 }
