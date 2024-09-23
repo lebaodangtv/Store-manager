a4bb58a56e6c0457a8bc78e07eec04b6270c5210
lebaodangtv <141621199+lebaodangtv@users.noreply.github.com>
2024-09-23 15:30:20 +0700

    Revert "update"
    This reverts commit 81285857c653c9a737114b8ebc5939a2e8520166.

--- src/main/java/com/websitebanhang/configuration/SecurityConfig.java
+++ src/main/java/com/websitebanhang/configuration/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.websitebanhang.configuration;
 
 import com.websitebanhang.enums.Role;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.jsonwebtoken.security.Keys;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,9 +14,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
 
 /**
  * cấu hình security trên method
@@ -29,11 +36,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private CustomJwtDecoder customJwtDecoder;
+    @NonFinal
+    @Value("${jwt.signerKey}")
+    private String key;
     private final String[] PUBLIC_ENDPOINT_GET = {"/v1/api/products"};
-    private final String[] PUBLIC_ENDPOINT_POST = {"/user/create", "/auth/login", "/user/introspect-request",
-            "/user/logout"};
+    private final String[] PUBLIC_ENDPOINT_POST = {"/user/create", "/auth/login", "/user/introspect-request"};
     private final String[] PRIVATE_ENDPOINT_ADMIN_GET = {"/user/find"};
 
     /**
@@ -53,7 +60,7 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(customJwtDecoder)
+                        oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 )
                 .csrf(AbstractHttpConfigurer::disable);
@@ -72,6 +79,12 @@ public class SecurityConfig {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
         return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKey secretKey = Keys.hmacShaKeyFor(key.getBytes());
+        return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS512).build();
     }
 
     @Bean
