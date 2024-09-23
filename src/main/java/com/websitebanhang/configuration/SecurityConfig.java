580798446eef934d38961e0692ca2763e758b566
lebaodangtv <141621199+lebaodangtv@users.noreply.github.com>
2024-09-23 15:31:10 +0700

    Revert "update"
    This reverts commit 9a44ada911672db598bf1423f7e781f53a4819bf.

--- src/main/java/com/websitebanhang/configuration/SecurityConfig.java
+++ src/main/java/com/websitebanhang/configuration/SecurityConfig.java
@@ -1,46 +1,23 @@
 package com.websitebanhang.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.crypto.spec.SecretKeySpec;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Value("${jwt.signerKey}")
-    private String key;
-
-    private final String [] PUBLIC_ENDPOINT = {"/v1/api/products"};
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(request ->
-            request.requestMatchers(HttpMethod.GET,PUBLIC_ENDPOINT).permitAll()
+            request.requestMatchers(HttpMethod.POST,"/v1/api/products/find").permitAll()
                     .anyRequest().authenticated()
-        );
-        http.oauth2ResourceServer(oauth2 ->
-                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
         );
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
-
-    @Bean
-    JwtDecoder jwtDecoder(){
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(),"HS512");
-        return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512)
-                .build();
-    }
-
 }
